18d1015329edebc63161094a34bec60d4676b695
MarceloCFSF <48691853+MarceloCFSF@users.noreply.github.com>
2019-11-19 13:31:38 -0300

    Biblioteca Usada
    Adicionei a biblioteca caso precise usar ewm um pc diferente

--- Nextion/Nextion_Final/Nextion_Final.ino
+++ Nextion/Nextion_Final/Nextion_Final.ino
@@ -1,29 +1,4 @@
-#include <doxygen.h>
-#include <NexButton.h>
-#include <NexCheckbox.h>
-#include <NexConfig.h>
-#include <NexCrop.h>
-#include <NexDualStateButton.h>
-#include <NexGauge.h>
-#include <NexGpio.h>
-#include <NexHardware.h>
-#include <NexHotspot.h>
-#include <NexNumber.h>
-#include <NexObject.h>
-#include <NexPage.h>
-#include <NexPicture.h>
-#include <NexProgressBar.h>
-#include <NexRadio.h>
-#include <NexRtc.h>
-#include <NexScrolltext.h>
-#include <NexSlider.h>
-#include <NexText.h>
-#include <NexTimer.h>
 #include <Nextion.h>
-#include <NexTouch.h>
-#include <NexUpload.h>
-#include <NexVariable.h>
-#include <NexWaveform.h>
 #include "MCP(ECU).h"
 
 #define button 7
