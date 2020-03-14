ccf99b6c7cc48ad4a1882719f1795577ce1ba61c
Marcelo CF <marcelo.cfsf@gmail.com>
2020-03-13 23:04:39 -0300

    Alterando estilo

--- Monitoramento-do-Carro/public/debug.js
+++ Monitoramento-do-Carro/public/debug.js
@@ -1,5 +1,6 @@
 var velocidade = {
     valor: document.querySelector(".velocidade span"),
+    aprox: 2,
     min: 0,
     max: 120
 }
@@ -8,6 +9,7 @@ genRandom(velocidade)
 
 var rpm = {
     valor: document.querySelector(".rpm span"),
+    aprox: 2,
     min: 0,
     max: 500
 }
@@ -15,6 +17,7 @@ genRandom(rpm)
 
 var map = {
     valor: document.querySelector(".map span"),
+    aprox: 2,
     min: 0,
     max: 120
 }
@@ -22,6 +25,7 @@ genRandom(map)
 
 var mat = {
     valor: document.querySelector(".mat span"),
+    aprox: 2,
     min: 0,
     max: 120
 }
@@ -29,6 +33,7 @@ genRandom(mat)
 
 var clt = {
     valor: document.querySelector(".clt span"),
+    aprox: 2,
     min: 0,
     max: 120
 }
@@ -36,13 +41,15 @@ genRandom(clt)
 
 var cgs = {
     valor: document.querySelector(".cgs span"),
+    aprox: 0,
     min: 0,
-    max: 3
+    max: 5
 }
 genRandom(cgs)
 
 var voltagem = {
     valor: document.querySelector(".voltagem span"),
+    aprox: 2,
     min: 0,
     max: 15
 }
@@ -50,6 +57,7 @@ genRandom(voltagem)
 
 var o2 = {
     valor: document.querySelector(".o2 span"),
+    aprox: 2,
     min: 0,
     max: 15
 }
@@ -57,6 +65,7 @@ genRandom(o2)
 
 var ECU = {
     valor: document.querySelector(".ECU span"),
+    aprox: 2,
     min: 0,
     max: 50
 }
@@ -64,7 +73,8 @@ genRandom(ECU)
 
 function genRandom(value) {
     var random = Math.random() * (+value.max - +value.min) + +value.min;
-    value.valor.innerHTML = parseFloat(random.toFixed(2))
+    value.valor.innerHTML = parseFloat(random.toFixed(value.aprox))
+    aprox: 2,
 
     setTimeout(() => {
         requestAnimationFrame(() => genRandom(value))
