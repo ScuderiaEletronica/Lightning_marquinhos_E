fd998bde9af994d75712a1baa2fd2c2ee2f0bc83
Marcelo CF <marcelo.cfsf@gamil.com>
2020-03-24 11:50:06 -0300

    Puxando dados da Firebase

--- Monitoramento-do-Carro/public/renderScreen.js
+++ Monitoramento-do-Carro/public/renderScreen.js
@@ -1,11 +1,17 @@
-export default function renderScreen(meters_database) {
+const ref = firebase.database().ref().child('meters')
 
-    for (const meterId in meters_database) {
-        const meter = meters_database[meterId]
-        document.querySelector('.' + meter.tag + ' span').innerHTML = meter.value
+ref.on('value', function (snap) {
+    console.log(snap.val())
+
+    for (const key in snap.val()) {
+        const meter = snap.val()[key];
+        const meterid = document.querySelector('.' + meter.tag + ' span')
+        meterid.innerHTML = meter.value
+        if(meter.value >= meter.max || meter.value <= meter.min){
+            meterid.classList.add("extremos")
+        }
+        else{
+            meterid.classList.remove("extremos")
+        }
     }
-
-    setTimeout(() => {
-        requestAnimationFrame(() => renderScreen(value))
-    }, 500);
-}
+})
